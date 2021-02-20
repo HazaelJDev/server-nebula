17b8803271ab5cd16cb214c90dd8c06678e9327a
Hazael Jiménez <viajero25057@innovaccion.mx>
2021-02-20 01:00:06 -0600

    update: route.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
 //Se importan los objetos que tienen las rutas de cada sección
-const apiAlumno = require('./users.routes');
+const apiUser = require('./users.routes');
 
 //Se le asigna la ruta base que le asiganara dentro de la api y se enlaza a el objeto que contiene las rutas de cada sección
 //router.use('/profesor',apiProfesor);
-router.use('/user',apiAlumno);
+router.use('/user',apiUser);
 
 module.exports = router;
