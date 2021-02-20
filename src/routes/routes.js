05c5f453c859dab88bce654596dd50f4a04651fa
Hazael Jiménez <viajero25057@innovaccion.mx>
2021-02-20 02:35:17 -0600

    add(routes.js): new routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 
 //Se importan los objetos que tienen las rutas de cada sección
 const apiUser = require('./users.routes');
+const apiRoom = require('./rooms.routes');
+const apiQuests = require('./quests.routes');
 
 //Se le asigna la ruta base que le asiganara dentro de la api y se enlaza a el objeto que contiene las rutas de cada sección
-//router.use('/profesor',apiProfesor);
 router.use('/user',apiUser);
+router.use('/room',apiRoom);
+router.use('/quests',apiQuests);
 
 module.exports = router;
